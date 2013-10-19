6602b9c331bbac289920c5601f37d5261cd8e1c5
drlech <drlech@gmail.com>
2013-10-20 01:18:31 +0200

    Powinien dzialac ajax sprawdzajacy czy sa wolne pokoje.

--- Hotel/views.py
+++ Hotel/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 # Jesli korzystamy ze skrotu ajax po prostu zwraca error.
 from django.http import HttpResponse, Http404
 
-from Hotel.models import Usluga, Pokoj
+from Hotel.models import Usluga, Pokoj, PokojNaRezerwacji, Rezerwacja
 
 
 def glowna(request):
@@ -23,6 +23,85 @@ def rezerwacje(request):
     })
 
 
+def wyszukaj_pokoje(poczatek_pobytu, koniec_pobytu, wymagane_pokoje):
+    return_status = {'pokoj1': 'not_checked', 'pokoj2': 'not_checked', 'pokoj3': 'not_checked'}
+
+    # Maksymalna pojemnosc pokoju
+    max_room_capacity = 0
+    for p in Pokoj.objects.all():
+        if p.rozmiar > max_room_capacity:
+            max_room_capacity = p.rozmiar
+
+    for i in range(1, wymagane_pokoje['ilosc'] + 1):
+        pokoj_i = 'pokoj%d' % (i,)
+
+        # Jesli pokoj ma 0 osob lub wiecej niz najwiekszy pokoj w hotelu
+        # zaznaczamy odpowiedni status
+        if wymagane_pokoje[pokoj_i] <= 0:
+            return_status[pokoj_i] = 'zero_selected'
+        elif wymagane_pokoje[pokoj_i] > max_room_capacity:
+            return_status[pokoj_i] = 'over_max_capacity'
+
+        # Pokoj ma normalna ilosc osob, szukamy czy jest wolny
+        else:
+
+            print 'POKOJ %d' % (i,)
+
+             # Stworzmy liste pk pokojow o odpowiednim rozmiarze
+            viable_rooms = []
+            for p in Pokoj.objects.all():
+                print '  iter pokoj %d' % (p.pk,)
+                if p.rozmiar == wymagane_pokoje[pokoj_i]:
+                    print '    viable_rooms + %d' % (p.pk,)
+                    viable_rooms.append(p.pk)
+
+            vr_print = ''
+            for room in viable_rooms:
+                vr_print += '%d, ' % (room,)
+            print vr_print
+
+            # Lecimy po rezerwacjach i jezeli istnieje taka rezerwacja ze w podanej dacie pokoj
+            # jest zajety to usuwamy go z listy
+            rooms_to_remove = []
+            for room in viable_rooms:
+                print '  iter viable_rooms %d' % (room,)
+                for r in Rezerwacja.objects.all():
+                    print '    iter reservation %s' % (r.email,)
+                    if poczatek_pobytu <= r.poczatek_pobytu <= koniec_pobytu or \
+                            koniec_pobytu >= r.koniec_pobytu >= poczatek_pobytu or \
+                            r.poczatek_pobytu <= poczatek_pobytu <= r.koniec_pobytu:
+                        print '      date collide'
+                        if r.pokojnarezerwacji_set.filter(pokoj__pk=room):
+                            print '      res collide with %d' % (room,)
+                            rooms_to_remove.append(room)
+
+            # Usuwamy znalezione zajete pokoje
+            for room in rooms_to_remove:
+                try:
+                    viable_rooms.remove(room)
+                except KeyError:
+                    pass
+
+            # Do tego trzeba usunac wszystkie pokoje ktore juz sa na liscie zwracanej
+            for key, value in return_status.items():
+                try:
+                    rpk = int(value)
+                    try:
+                        viable_rooms.remove(rpk)
+                    except KeyError:
+                        pass
+                except ValueError:
+                    pass
+
+            # Jesli zostaly nam jakies wolne pokoje to mozna pierwszy zwrocic jako wolny
+            if len(viable_rooms) > 0:
+                return_status[pokoj_i] = viable_rooms[0]
+            else:
+                return_status[pokoj_i] = 'no_free_rooms'
+
+    return return_status
+
+
 def rezerwacje_check(request):
     if request.is_ajax():
 
@@ -35,37 +114,30 @@ def rezerwacje_check(request):
 
             # Slownik z wymaganymi rozmiarami pokojow
             wymagane_pokoje = {'ilosc': int(request.GET['iloscPokojow'])}
-            dostepne_pokoje = {}
             for i in range(1, wymagane_pokoje['ilosc'] + 1):
                 wymagane_pokoje['pokoj%d' % (i,)] = int(request.GET['dorosli%d' % (i,)]) + int(request.GET['dzieci%d' % (i,)])
-                dostepne_pokoje['pokoj%d' % (i,)] = False
-
-            # Szukamy czy pokoje o wymaganych rozmiarach sa w bazie
-            for i in range(1, wymagane_pokoje['ilosc'] + 1):
-                for p in Pokoj.objects.all():
-                    if p.rozmiar == wymagane_pokoje['pokoj%d' % (i,)]:
-                        dostepne_pokoje['pokoj%d' % (i,)] = True
-                        break
 
             # Tymczasowy wynik
-            response = ''
-            sample_list = []
-            sample_list.append(1)
-            sample_list.append(2)
-            for l in sample_list:
-                response += '%d, ' % l
+            list_of_pks = wyszukaj_pokoje(poczatek_pobytu, koniec_pobytu, wymagane_pokoje)
+
+            response = '{'
+            for i in range(1, wymagane_pokoje['ilosc'] + 1):
+                if i > 1:
+                    response += ', '
+                response += '"pokoj%d": "%s"' % (i, str(list_of_pks['pokoj%d' % (i,)]))
+            response += '}'
 
         # Czegos brakuje w requescie
-        except KeyError:
-            response = 'Dict error'
+        except KeyError, key:
+            response = key
 
         # Cos w requescie ma nieprawidlowy typ
         except SyntaxError:
             response = 'Parse error'
 
         # Nieprawidlowy format, np data 10/1234/2013
-        except ValueError:
-            response = 'Value error'
+        except ValueError, value:
+            response = value
 
         return HttpResponse(response)
     else:
