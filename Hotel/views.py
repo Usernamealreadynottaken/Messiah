62c179782e123af95c0a5257ce4fcea128deaf41
drlech <drlech@gmail.com>
2013-10-19 19:15:33 +0200

    Czesciowo dzialajacy ajax.

--- Hotel/views.py
+++ Hotel/views.py
@@ -1,11 +1,12 @@
+import datetime
+from datetime import date
 from django.shortcuts import render
 
 # Zeby skorzystac z ajaxa potrzebujemy zwrocic HttpResponse object.
 # Jesli korzystamy ze skrotu ajax po prostu zwraca error.
 from django.http import HttpResponse, Http404
-from django.template import loader, RequestContext
 
-from Hotel.models import Pokoj
+from Hotel.models import Usluga, Pokoj
 
 
 def glowna(request):
@@ -13,22 +14,59 @@ def glowna(request):
 
 
 def rezerwacje(request):
-    return render(request, 'hotel/rezerwacje.html', {'pokoje': Pokoj.objects.all()})
+    uslugi_wewnetrzne = Usluga.objects.filter(zewnetrzna=False)
+    uslugi_zewnetrzne = Usluga.objects.filter(zewnetrzna=True)
+    return render(request, 'hotel/rezerwacje.html', {
+        'uslugi_all': Usluga.objects.all(),
+        'uslugi_wewnetrzne': uslugi_wewnetrzne,
+        'uslugi_zewnetrzne': uslugi_zewnetrzne
+    })
 
 
 def rezerwacje_check(request):
     if request.is_ajax():
-        # Ten sam template jest zwracany niezaleznie czy byl blad czy nie
-        template = loader.get_template('hotel/rezerwacje_message.html')
 
-        response = {}
         try:
-            pokoj = int(request.GET['pokoj'])
-            response['pokoj'] = pokoj
-        except ValueError:
-            response['error_message'] = 'Brak zaznaczonych pokojow'
+            # Konwersja dat ze stringow do date
+            poczatek_pobytu_split = request.GET['poczatekPobytu'].split('/')
+            koniec_pobytu_split = request.GET['koniecPobytu'].split('/')
+            poczatek_pobytu = date(int(poczatek_pobytu_split[2]), int(poczatek_pobytu_split[0]), int(poczatek_pobytu_split[1]))
+            koniec_pobytu = date(int(koniec_pobytu_split[2]), int(koniec_pobytu_split[0]), int(koniec_pobytu_split[1]))
 
-        context = RequestContext(request, response)
-        return HttpResponse(template.render(context))
+            # Slownik z wymaganymi rozmiarami pokojow
+            wymagane_pokoje = {'ilosc': int(request.GET['iloscPokojow'])}
+            dostepne_pokoje = {}
+            for i in range(1, wymagane_pokoje['ilosc'] + 1):
+                wymagane_pokoje['pokoj%d' % (i,)] = int(request.GET['dorosli%d' % (i,)]) + int(request.GET['dzieci%d' % (i,)])
+                dostepne_pokoje['pokoj%d' % (i,)] = False
+
+            # Szukamy czy pokoje o wymaganych rozmiarach sa w bazie
+            for i in range(1, wymagane_pokoje['ilosc'] + 1):
+                for p in Pokoj.objects.all():
+                    if p.rozmiar == wymagane_pokoje['pokoj%d' % (i,)]:
+                        dostepne_pokoje['pokoj%d' % (i,)] = True
+                        break
+
+            # Tymczasowy wynik
+            response = ''
+            sample_list = []
+            sample_list.append(1)
+            sample_list.append(2)
+            for l in sample_list:
+                response += '%d, ' % l
+
+        # Czegos brakuje w requescie
+        except KeyError:
+            response = 'Dict error'
+
+        # Cos w requescie ma nieprawidlowy typ
+        except SyntaxError:
+            response = 'Parse error'
+
+        # Nieprawidlowy format, np data 10/1234/2013
+        except ValueError:
+            response = 'Value error'
+
+        return HttpResponse(response)
     else:
         raise Http404
